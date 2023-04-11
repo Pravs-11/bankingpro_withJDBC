555eeb1bd721667141215d6dcd5b7220fff2b11c
Pravs_11 <thotapalli9866@gmail.com>
2023-04-11 21:42:41 +0530

    Second day of jdbc

--- src/main/java/com/bankingpro/bankingdao.java
+++ src/main/java/com/bankingpro/bankingdao.java
@@ -3,12 +3,12 @@ package com.bankingpro;
 import java.sql.*;
 public class bankingdao {
 	Connection con=null;
+	//method to get connection with Data base
 	public void dbconnection() throws Exception{
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bankingpro", "root","Pravs@99");
-		
-		
+		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/bankingpro", "root","Pravs@99");	
 	}
+//Method to register customer to data base
 	public int registercustomer(customer c1)throws Exception {
 		String query="insert into customer values(?,?,?,?)";
 		PreparedStatement pst=con.prepareStatement(query);
@@ -18,6 +18,49 @@ public class bankingdao {
 		pst.setInt(4,c1.cusamount);
 		int response=pst.executeUpdate();
 	return response;
+	}
+	//method to login the customer
+	public int login(String uname, int pwd)throws Exception{
+		String query="select * from customer where cusname='"+uname+"'";
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery(query);
+		//checking whether the customer details present or not
+		if(rs.next()) {
+			//fetching the password from db
+			int password=rs.getInt(3);
+			
+			//matching the password
+			if(password==pwd) {
+				//login success
+				return rs.getInt(1);
+			}
+			else {
+				//password incorrect
+				return 0;
+			}
+		}
+		else {
+			//unable to fetch the details
+			return -1;
+		}
+	
+	}
+	public int deposit(int amount, int customerid)throws Exception{
+		//fetching user details based on customer id
+		String query2="select * from customer where cusid="+customerid;
+		Statement st=con.createStatement();
+		ResultSet rs=st.executeQuery(query2);
+		rs.next();
+		//extracting account balance
+		int bal=rs.getInt(4);
+		//updating amount
+		amount+=bal;
+		//storing updated amount
+		String query="update customer set cusamount="+amount+" where cusid="+customerid;
+		PreparedStatement pst=con.prepareStatement(query);
+		pst.executeUpdate();
+		//return updated amount
+		return amount;
 	}
 	
 
--- src/main/java/com/bankingpro/bankingmain.java
+++ src/main/java/com/bankingpro/bankingmain.java
@@ -10,13 +10,20 @@ Scanner bs=new Scanner(System.in);
 bankingdao dao=new bankingdao();
 customer c1=new customer();
 System.out.println("\t/t******Welcome to HDFC BANK***");
-System.out.println("Enetr Customer name");
+
+System.out.println("Press 1 for Registration \n Press 2 for login");
+int operation=bs.nextInt();
+switch(operation) {
+case 1->{
+
+System.out.println("Enter Customer name");
+bs.nextLine();
 String cname=bs.nextLine();
-System.out.println("Enter cus id");
+System.out.println("Enter customer id");
 int cid=bs.nextInt();
-System.out.println("Enter cus pin");
+System.out.println("Enter customer pin");
 int cpin=bs.nextInt();
-System.out.println("Enter cus amount");
+System.out.println("Enter customer amount");
 int cbal=bs.nextInt();
 c1.cusid=cid;
 c1.cusname=cname;
@@ -25,12 +32,51 @@ c1.cusamount=cbal;
 dao.dbconnection();
 int res =dao.registercustomer(c1);
 if(res==1) {
-	System.out.println("acc success");
+	System.out.println("account registration successful");
 }
 else {
-	System.out.println("wrong");
+	System.out.println("Error, Not able to register");
+}
+}
+case 2->{
+	System.out.println("Welcome to login page");
+	//Reading username and password from login
+	System.out.println("Enter username");
+	bs.nextLine();
+	String uname=bs.nextLine();
+	System.out.println("Enter Password");
+	int pwd=bs.nextInt();
+	//get connection with data base
+	dao.dbconnection();
+	//login method
+	int res=dao.login(uname, pwd);
+	//handling the response
+	if(res==0) {
+		System.out.println("username/password is incorrect");	
+	}
+	else if(res==-1) {
+		System.out.println("Unable to find the customer details");	
+		
+	}
+	else {
+		System.out.println("Login successful");
+		System.out.println("Press 1 for Deposit\nPress 2 for Withdraw\nPress 3 for change password\nPress 4 for Delete Acc");
+		int process=bs.nextInt();
+		switch(process) {
+		case 1->{
+			System.out.println("Enter amount to deposit");
+			int amount=bs.nextInt();
+			int bal=dao.deposit(amount,res);
+			System.out.println("Deposit successful\n Available balance is : "+bal);
+		}
+		}
+	}
+}
 }
 bs.close();
+
+
+
 	}
 
 }
